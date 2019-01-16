919a19051b4ffd4eb63310801bbb7b58911add69
jkjnmlk <123@example.com>
2019-01-16 11:51:19 +0800

    添加了删除与显示功能

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main()
 {
 
     printf("敏感字符管理系统\n");
+    int code;
 
     while (1)
     {
@@ -14,38 +15,52 @@ int main()
         printf("4--替换所有敏感字符\n");
         printf("5--退出系统\n");
         printf("请输入所需项目数字\n");
-        int code;
+
         scanf("%d", &code);
 
-        if (code = 1)
+        if (code==1)
         {
             printf("请输入要添加的敏感字符\n");
             char m;
             scanf("%c", &m);
-            scanf("%c", &m);//消除空格的影响
-            arr[index]=m;
+            scanf("%c", &m); //消除空格的影响
+            arr[index] = m;
             index++;
             printf("添加成功，点击回车继续\n");
             char x;
             scanf("%c", &x);
             scanf("%c", &x);
         }
-        if (code = 2)
+        if (code==2)
         {
-            /* code */
+            index--; //此处含有一个bug
+            printf("删除成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
 
-        if (code = 3)
+        if (code==3)
+        {
+           
+           for(int i = 0; i < index; i++)
+           {
+               printf("第%d个字符为%c\n",i+1,arr[i]);
+           }
+            printf("点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
+           
+        }
+        if (code==4)
         {
             /* code */
         }
-        if (code = 4)
+        if (code==5)
         {
-            /* code */
-        }
-        if (code = 5)
-        {
-            /* code */
+            printf("点击回车，退出系统\n");
+            break;
         }
     }
 
